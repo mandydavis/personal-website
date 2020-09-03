38d2f5837b28c00aa75e5dce6cf3046cac2886c2
Mandy Davis <amandadavis2021@u.northwestern.edu>
2020-09-03 13:51:02 -0500

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-b571b88bf4edac971eac.js"
+    "url": "webpack-runtime-753ca7f23ad906831b66.js"
   },
   {
     "url": "framework-dab657cf2d102cb8d2b9.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "a3c499920cc2ad186c46b8c6226c1aab"
+    "revision": "82257e791e30f6f77f2fcf08d2f52c1e"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-5a1ee105f05b3144b807.js"
